89a21abec1d3eb659c8a68365a5d96f535f66d21
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-21 13:16:13 +0530

    Add files via upload

---
+++ Albhabet_case_checker.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+
+  char character ;
+
+  printf("Enter a character : ");
+  scanf("%c",&character);
+
+  if(character >= 'a' && character <= 'z'){
+
+        printf("The character you Entered is Lowercase");
+
+
+  }
+
+  else{
+
+       printf("The character you Entered is Uppercase");
+
+  }
+
+
+
+
+
+
+
+}
